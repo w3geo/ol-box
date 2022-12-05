cf8cd0793ea3ff9960c5dfd165f8e32dea8518f5
Andreas Hocevar <andreas.hocevar@gmail.com>
2022-12-05 19:54:28 +0100

    Add config to make vite build work

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,4 +1,15 @@
+import { globbySync } from 'globby'; // eslint-disable-line import/no-extraneous-dependencies
+
+const inputs = globbySync('micro-apps/**/*.html');
+
 /** @type {import('vite').UserConfig} */
 export default {
-
+  build: {
+    rollupOptions: {
+      input: inputs.reduce((acc, input) => {
+        acc[input] = input;
+        return acc;
+      }, { index: 'index.html' }),
+    },
+  },
 };
